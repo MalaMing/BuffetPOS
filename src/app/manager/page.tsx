063d74714c228782d699c847054fc8c013e2f7b1
HomieZ09 <phumsoen123@gmail.com>
2024-10-25 13:19:31 +0700

    init commit

---
+++ src/app/manager/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <div className="px-2 mt-2 space-y-3 pt-3 pb-24">
+        hello world
+      </div>
+    </div>
+  );
+}
