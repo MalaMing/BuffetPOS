a9d86221d482f6dc6d0ce0a9d4b060c25a6e452b
HomieZ09 <phumsoen123@gmail.com>
2024-11-02 23:45:33 +0700

    init user

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,9 +1,19 @@
+'use client';
+
 import ScreenMobile from "@/components/ScreenMobile";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const searchParams = useSearchParams()
+
+  useEffect(() => {
+    console.log(searchParams.get('accessKey'))
+  }, [searchParams])
+
   return (
     <ScreenMobile>
-      Helo world
+      {searchParams.get('accessKey')}
     </ScreenMobile>
   );
 }
