f3864d2b47e00323e35d79ae686b4f11de045cd0
Malaming <minggggggg@gmail.com>
2024-11-03 18:31:43 +0700

    add user-history page

---
+++ src/components/user/OrderedItemsList.tsx
@@ -0,0 +1,9 @@
+import OrderedItems from "./OrderedItems";
+
+export default function OrderedItemsList() {
+    return(
+        <div className="flex flex-col gap-2" >
+            {[1,2,3,4].map((item,index) => <OrderedItems key={index} />) }
+        </div>
+    );
+}
