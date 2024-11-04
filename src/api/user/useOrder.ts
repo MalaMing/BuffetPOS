7051cc27592c79a661580d69e04c05be4f0c6f12
HomieZ09 <phumsoen123@gmail.com>
2024-11-04 19:09:16 +0700

    Add order management API and define order interfaces

---
+++ src/api/user/useOrder.ts
@@ -0,0 +1,16 @@
+import { useMutation, useQuery } from "@tanstack/react-query";
+import axiosInstance from "@/lib/axiosInstance";
+import { getSession } from "next-auth/react";
+import { OrderRequest } from "@/interfaces/order";
+
+const addOrders = async (newOrders: OrderRequest) => {
+    const session = await getSession();
+    const { data } = await axiosInstance.post("/customer/orders", newOrders, {
+        headers: {
+            AccessCode: `Bearer ${session?.token}`,
+        },
+    });
+    return data;
+}
+
+export{ addOrders }
