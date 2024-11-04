ec6afa688412d448d327c311355dfaf8138b0d2f
HomieZ09 <phumsoen123@gmail.com>
2024-11-04 19:30:07 +0700

    Enhance order management: add access code handling and new hooks for adding and retrieving orders

--- src/api/user/useOrder.ts
+++ src/api/user/useOrder.ts
@@ -1,16 +1,36 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import axiosInstance from "@/lib/axiosInstance";
-import { getSession } from "next-auth/react";
 import { OrderRequest } from "@/interfaces/order";
 
-const addOrders = async (newOrders: OrderRequest) => {
-    const session = await getSession();
+const addOrders = async (newOrders: OrderRequest, accessCode: string) => {
     const { data } = await axiosInstance.post("/customer/orders", newOrders, {
         headers: {
-            AccessCode: `Bearer ${session?.token}`,
+            AccessCode: accessCode,
         },
     });
     return data;
 }
 
-export{ addOrders }
+const getOrder = async (accessCode: string) => {
+    const { data } = await axiosInstance.get("lor api jaw", {
+        headers: {
+            AccessCode: accessCode,
+        },
+    });
+    return data;
+}
+
+const useAddOrders = (newOrders: OrderRequest, accessCode: string) => {
+    return useMutation({
+        mutationFn: ()=> addOrders(newOrders, accessCode),
+    });
+}
+
+const useGetOrder = (accessCode: string) => {
+    return useQuery({
+        queryKey: ["orders", accessCode],
+        queryFn: ()=> getOrder(accessCode),
+    });
+}
+
+export{ useAddOrders, useGetOrder }
