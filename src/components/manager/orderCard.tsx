032fc0d6fa854ce389b08b1f4816572dcd22c609
xeff09 <wongchaita03@gmail.com>
2024-11-05 01:11:42 +0700

    complete: current order page integration

--- src/components/manager/orderCard.tsx
+++ src/components/manager/orderCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from "@/components/ui/button";
 import { OrderItemResponse } from '@/interfaces/order';
 import Image from "next/image";
@@ -8,39 +8,33 @@ import LoadingAnimation from './loadingAnimation';
 import { useGetMenuByID, useGetMenus } from '@/api/manager/useMenu';
 import { useGetCategoryById } from '@/api/manager/useCategory';
 
-
-
 export default function OrderCard({ orderItem }: {orderItem: OrderItemResponse}) {
-    const { data: menu , isLoading: loadingMenus } = useGetMenuByID(orderItem.menuID);
-    const { data: category, isLoading: loadingCategory } = menu?.categoryId 
-        ? useGetCategoryById(menu.categoryId) 
-        : { data: null, isLoading: false };
+    
+    const { data: menu, isLoading: loadingMenus } = useGetMenuByID(orderItem.menuID);
+    const { data: category, isLoading: loadingCategory } = useGetCategoryById(menu?.categoryId || '');
 
     if (loadingMenus) {
-        return <LoadingAnimation/>
+        return <LoadingAnimation />;
     }
-
 
     return (
         <div className="flex flex-col w-48 shadow-md m-2 p-2 rounded-lg gap-3">
             <figure className="h-full w-full">
                 <Image
                     src={menu?.imageUrl || "/default-image.png"}
-                    alt={menu?.name|| "Menu Image"}
+                    alt={menu?.name || "Menu Image"}
                     width={100}
                     height={100}
-                    layout=""
                     className="object-cover w-full h-full"
                 />
             </figure>
-          <div className="w-full flex flex-col">
-            <h2 className="card-title">{menu?.name}</h2>
-            <div>
-                <h2 className="card-title">{category?.name || "No Category"}</h2>
+            <div className="w-full flex flex-col">
+                <h2 className="card-title">{menu?.name}</h2>
+                <div>
+                    <h2 className="card-title">{loadingCategory ? "Loading..." : category?.name || "No Category"}</h2>
+                </div>
             </div>
-          </div>
-          <div className="w-full justify-end flex flex-row">x1</div>
+            <div className="w-full justify-end flex flex-row">x1</div>
         </div>
-      );
-    
+    );
 }
