94c59f6e942d09679a94cc0d786cf6b1adc8f03c
Malaming <minggggggg@gmail.com>
2024-11-04 18:27:04 +0700

    link page and pop-up confirmation of user

--- src/app/user/cart/page.tsx
+++ src/app/user/cart/page.tsx
@@ -1,10 +1,20 @@
+'use client';
+import { ConfirmDialog } from "@/components/manager/confirmDialog";
+import { ConfirmOrderDialog } from "@/components/user/ConfirmOrderDialog";
 import MenuCardList from "@/components/user/MenuCardList";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Cart() {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const router = useRouter();
+
     return (<div className="flex flex-col">
         <div className=" flex flex-row pl-4 pt-4">
-            <div><Icon icon="weui:back-filled" fontSize={40} className="" /></div>
+            <p ><Icon icon="weui:back-filled" fontSize={40} onClick={() => router.back()} /></p>
             <p className="w-full text-2xl  text-center font-semibold pt-1">ตะกร้าของฉัน</p>
         </div>
         <div className="flex px-4 pt-5 pb-23" >
@@ -15,10 +25,12 @@ export default function Cart() {
             <p className=" text-xl font-bold "> 100 รายการ</p>
         </div>
         <div className="flex pt-6 justify-center pb-6">
-            <button className="bg-primary text-white font-bold text-lg py-3 px-4 rounded-lg w-11/12 pb-4 shadow-lg">
+            <button onClick={() => setIsOpen((o) => !o)} className="bg-primary text-white font-bold text-lg py-3 px-4 rounded-lg w-11/12 pb-4 shadow-lg">
                 สั่งอาหารเลย
             </button>
         </div >
+        <ConfirmOrderDialog openDialog={isOpen} setOpenDialog={setIsOpen} title="ยืนยันการสั่งอาหาร" description="กรุณากดยืนยันเพื่อสั่งอาหาร" callback={() => router.push('/user/history')}/>
 
     </div>);
+
 }
--- src/app/user/history/page.tsx
+++ src/app/user/history/page.tsx
@@ -1,18 +1,25 @@
+'use client';
 import OrderedItemsList from "@/components/user/OrderedItemsList";
 import { Icon } from "@iconify/react/dist/iconify.js";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 
 export default function History() {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
     return (
         <div className="flex flex-col">
             <div className="pl-3 pt-6 pb-1">
-                <Icon icon="weui:back-filled" fontSize={40} />
+                <Icon icon="weui:back-filled" fontSize={40} onClick={() => router.back()}/>
                 <div className="flex flex-row pt-3 mb-3 border-b border-b-whereBlack pb-2 w-10/12 mx-auto">
                 <Icon icon="lets-icons:order-fill" fontSize={25}  />
                 <p className="text-xl font-bold"> รายการอาหารที่สั่งแล้ว</p>
                 </div>
             </div>
             <OrderedItemsList />
-            <button className="bg-primary text-white font-bold text-lg py-3 px-4 mt-5 rounded-lg w-11/12 pb-3 shadow-lg self-center">
+            <button onClick= {()=> router.replace('/user')}className="bg-primary text-white font-bold text-lg py-3 px-4 mt-5 rounded-lg w-11/12 pb-3 shadow-lg self-center">
                 สั่งอาหารเพิ่ม
             </button>
             <div className="pl-3 pt-3">
